6e3cd1f2484290a6ee64d315e19ff99d7e88ee3c
Joubert Vasconcelos <joubert@atmsistema.com.br>
2025-09-09 11:17:27 -0300

    ConnectionPool type

---
+++ dist/db/interfaces/ICrudEvent.d.ts
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) 2025, Joubert Vasconcelos
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import { ResultSetHeader } from 'mysql2';
+import { ILoggedUser } from './ILoggedUser';
+export interface ICrudEvent {
+    command: string;
+    values: any;
+    inTransaction: boolean;
+    result: ResultSetHeader;
+    user: ILoggedUser;
+}
