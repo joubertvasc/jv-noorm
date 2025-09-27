1856443c6b7bdc0154c3f514b4ba4364e74b4f10
Joubert Vasconcelos <joubert@atmsistema.com.br>
2025-09-27 18:46:30 -0300

    Better crud error management

---
+++ dist/shared/errors/value-already-exists-on-parent-error.js
@@ -0,0 +1,16 @@
+"use strict";
+/**
+ * Copyright (c) 2025, Joubert Vasconcelos
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ValueAlreadyExistsOnParentError = void 0;
+class ValueAlreadyExistsOnParentError extends Error {
+    constructor(message) {
+        super(message ?? 'value-already-not-exists-on-parent-error');
+    }
+}
+exports.ValueAlreadyExistsOnParentError = ValueAlreadyExistsOnParentError;
